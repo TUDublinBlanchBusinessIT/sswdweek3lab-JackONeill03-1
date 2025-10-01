aa10b00548ace2d3e0f84d8d558ebbb390de91b2
B00155573 Jack O Neill <B00155573@mytudublin.ie>
2025-10-01 10:50:24 +0100

    Step 3 15x20 Grid

---
+++ nestedForLoop.php
@@ -0,0 +1,13 @@
+<?PHP
+echo "<TABLE border= '1'>";
+for ($i=1; $i<=15; $i++) {
+	echo "<TR>";
+    
+    for ($j=1; $j<=20; $j++) {
+        echo "<TD>$i, $j</TD>";
+    
+    }
+}
+echo "</TR>"
+
+?>
